ca5b8498de14f3c9fbe459d265a9fabf8fd4686c
sicongtang <bobby3239@gmail.com>
2014-10-23 23:38:56 +0800

    careercup trees and graphs

---
+++ src/main/java/com/careercup/crack/chapter4/Q1.java
@@ -0,0 +1,15 @@
+package com.careercup.crack.chapter4;
+
+/**
+ * Implement a function to check if a tree is balanced For the purposes of this
+ * question, a balanced tree is defined to be a tree such that no two leaf nodes
+ * differ in distance from the root by more than one
+ * 
+ * @author BobbyTang
+ *
+ */
+public class Q1 {
+
+	
+	
+}
