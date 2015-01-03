f981d016893a23657eb5a35ef2d28e7e5c41ebef
BobbyTang <bobby3239@gmail.com>
2015-01-03 22:52:01 +0800

    tree question

---
+++ src/main/java/com/careercup/crack/chapter4/Q5.java
@@ -0,0 +1,12 @@
+package com.careercup.crack.chapter4;
+
+/**
+ * Write an algorithm to find the ‘next’ node (i.e., in-order successor) of a
+ * given node in a binary search tree where each node has a link to its parent.
+ * 
+ * @author BobbyTang
+ *
+ */
+public class Q5 {
+	
+}
---
+++ src/main/java/com/careercup/crack/chapter4/Q6.java
@@ -0,0 +1,13 @@
+package com.careercup.crack.chapter4;
+
+/**
+ * Design an algorithm and write code to find the first common ancestor of two
+ * nodes in a binary tree. Avoid storing additional nodes in a data structure.
+ * NOTE: This is not necessarily a binary search tree.
+ * 
+ * @author BobbyTang
+ *
+ */
+public class Q6 {
+
+}
